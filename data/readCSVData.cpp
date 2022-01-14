4de83888005857a9ef8e985ed162b1c09442ec7f
Tony <ashelto6@kent.edu>
2022-01-14 15:01:54 -0500

    removed profiling tools for debugging

--- data/readCSVData.cpp
+++ data/readCSVData.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <vector>
 #include <unordered_map>
-#include <sstream>
 
 #include "readCSVData.hpp"
 
@@ -22,8 +21,6 @@ std::vector<std::string> Read::readVidIds(std::string filePath)
     return vidIds;
 }
 
-#include <chrono>
-#include <thread>
 std::vector<std::string> Read::readVidNames(std::string filePath)
 {
     std::string line;
@@ -32,32 +29,10 @@ std::vector<std::string> Read::readVidNames(std::string filePath)
 
     std::ifstream file(filePath);
 
-    /*
-    using std::chrono::high_resolution_clock;
-    using std::chrono::duration_cast;
-    using std::chrono::duration;
-    using std::chrono::milliseconds;
-
-    auto t1 = high_resolution_clock::now();
-    */
-
     while( getline(file,line) )
         vidNames.push_back(line.substr(line.find_first_of(',')+1, line.length())); //stores video names from csv
         //vidIds.push_back(line.substr(0, line.find_first_of(','))); //stores video Ids from csv
     file.close();
-
-    /*
-    auto t2 = high_resolution_clock::now();
-
-    // Getting number of milliseconds as an integer. //
-    auto ms_int = duration_cast<milliseconds>(t2 - t1);
-
-    // Getting number of milliseconds as a double. //
-    duration<double, std::milli> ms_double = t2 - t1;
-
-    std::cout << ms_int.count() << "ms\n";
-    std::cout << ms_double.count() << "ms\n";
-    */
 
     return vidNames;
 }
@@ -69,25 +44,12 @@ std::unordered_map<std::string, std::vector<int>> Read::readProfileRatings(std::
 
     std::fstream file(filePath, std::ios::in);
 
-    /*    
-    using std::chrono::high_resolution_clock;
-    using std::chrono::duration_cast;
-    using std::chrono::duration;
-    using std::chrono::milliseconds;
-
-    auto t1 = high_resolution_clock::now();
-    */   
-
     while( getline(file, line) )
     { 
         std::string userName = line.substr(0, line.find_first_of(','));
         line.erase(0, line.find_first_of(',') + 1);
-        std::string delimiter = ",";
-
-        size_t pos = 0;
-        std::string token;
         
-        for( int i = 0; i < line.length(); ++i )
+        for( size_t i = 0; i < line.length(); ++i )
         {
             if( line[i] != ',' && line[i] == '-' )
             {   
@@ -99,19 +61,6 @@ std::unordered_map<std::string, std::vector<int>> Read::readProfileRatings(std::
         }
     }
     file.close(); 
-
-    /*  
-    auto t2 = high_resolution_clock::now();
-
-    // Getting number of milliseconds as an integer. //
-    auto ms_int = duration_cast<milliseconds>(t2 - t1);
-
-    // Getting number of milliseconds as a double. //
-    duration<double, std::milli> ms_double = t2 - t1;
-
-    std::cout << ms_int.count() << "ms\n";
-    std::cout << ms_double.count() << "ms\n";
-    */
 
     //std::cout << profileRatings["Tony"].size() << "\n";
     return profileRatings;
