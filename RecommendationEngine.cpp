bf689ad7c9ab504894437dc53ecdc44bb4d80174
Tony <ashelto6@kent.edu>
2022-01-14 15:01:38 -0500

    added caching, helper methods, and multithreading

--- RecommendationEngine.cpp
+++ RecommendationEngine.cpp
@@ -1,60 +1,81 @@
 /*
 * Author: Tony S-M
 * Date: 12/10/2021
-
+*
 * A RecommendationEngine class definition
 *
 * Using collaborative filtering
 */
 
+//#include <chrono>
 #include <iostream>
+#include <iomanip>
 #include <algorithm>
-#include <numeric>
 #include <vector>
+#include <array>
+#include <iterator>
+#include <cmath>
+#include <thread>
+#include <memory>
 #include <unordered_map>
 
 #include "RecommendationEngine.hpp"
 #include "data/readCSVData.hpp"
 
-vecOfStrToDoubPairs RecommendationEngine::getSimilarUsers(const std::string& targetUser, int k)
+vecOfStrToDoubPairs RecommendationEngine::getSimilarUsers(const std::string& targetUser, int k /*= 3*/)
 {
+    if( k < 1 )
+        k = 1;
+    else if( k >= profiles_->size() )
+        k = profiles_->size()-1;
+    
     vecOfStrToDoubPairs similarUsers;
 
-    std::for_each(profiles_->begin(), profiles_->end(), [&](auto& profile){
+    std::for_each(profiles_->begin(), profiles_->end(), [&](auto& profile)
+    {
         if( profile.first != targetUser )
-            similarUsers.push_back({profile.first, getSimilarityIndex(targetUser, profile.first)});
+            similarUsers.push_back({profile.first, similarityIndex(targetUser, profile.first)});
     });
 
-    std::sort(similarUsers.begin(), similarUsers.end(), [](auto& user1, auto& user2){
+    std::partial_sort(similarUsers.begin(), similarUsers.begin()+3, similarUsers.end(), [](auto& user1, auto& user2)
+    {
         return user1.second > user2.second; 
     });
     similarUsers.resize(k);
-/*
+    
+    /*
     std::cout << "\nSimilar Users for " << targetUser << ": \n";
     for( auto& user : similarUsers )
         std::cout << user.first << ": " << user.second << "\n";
     std::cout << "\n";
-*/
+    */
+
     return similarUsers;
 }
 
-vecOfStrToDoubPairs RecommendationEngine::getRecommendations(const std::string& user, int k, vecOfStrToDoubPairs simUsers /*= vecOfStrToDoubPairs()*/)
+vecOfStrToDoubPairs RecommendationEngine::getRecommendations(const std::string& targetUser, int k, vecOfStrToDoubPairs simUsers /*= vecOfStrToDoubPairs()*/)
 {
-    vecOfStrToDoubPairs vids, similarUsers;
+    if( k < 1 )
+        k = 1;
+    else if( k > unseen_[targetUser].size() )
+        k = unseen_[targetUser].size();
     
-    similarUsers = ( !simUsers.size() ) ? getSimilarUsers(user) : simUsers;
+    vecOfStrToDoubPairs vids, similarUsers;
 
-    //std::cout << "Video Scores:\n";
-    std::cout << unseen_[user].size() << "\n" << videos_->size();
-    for( size_t i = 0; i < unseen_[user].size(); ++i )
+    similarUsers = ( !simUsers.size() ) ? getSimilarUsers(targetUser) : simUsers;
+    /*
+    std::cout << "Video Scores:\n";
+    std::cout << unseen_[targetUser].size() << "\n" << videos_->size();
+    */
+    for( size_t i = 0; i < unseen_[targetUser].size(); ++i )
     {
-        //std::cout << (*profiles_)[user][unseen_[user][i]] << "\n";
         double vidScore = 0.0;
         int numOfScores = 0;
-        std::for_each( similarUsers.begin(), similarUsers.end(), [&](auto& profile){
-            if( (*profiles_)[profile.first][unseen_[user][i]] != 0 )
+        std::for_each( similarUsers.begin(), similarUsers.end(), [&](auto& profile)
+        {
+            if( isZero((*profiles_)[profile.first][unseen_[targetUser][i]]) == false && isZero(profile.second) == false )
             {      
-                vidScore += (profile.second * (*profiles_)[profile.first][unseen_[user][i]]);
+                vidScore += (profile.second * (*profiles_)[profile.first][unseen_[targetUser][i]]);
                 ++numOfScores;
             }
         });
@@ -67,78 +88,249 @@ vecOfStrToDoubPairs RecommendationEngine::getRecommendations(const std::string& 
             */
             vidScore /= numOfScores;
         }
-        //std::cout << "video" << i+1 << " score: " <<  vidScore << "\n";
-        vids.push_back({(*videos_)[unseen_[user][i]], vidScore});
+        //std::cout << "video" << unseen_[targetUser][i]+1 << " score: " <<  vidScore << "\n";
+        vids.push_back({(*videos_)[unseen_[targetUser][i]], vidScore});
     }
 
-    std::sort(vids.begin(), vids.end(), [](auto& video1, auto& video2){ 
+    std::partial_sort(vids.begin(), vids.begin()+k, vids.end(), [](auto& video1, auto& video2)
+    { 
         return video1.second > video2.second; 
-        });
+    });
     vids.resize(k);
     //std::cout << "\n";
 
     return vids;
 }
 
-double RecommendationEngine::getSimilarityIndex(const std::string& user_a, const std::string& user_b)
+double RecommendationEngine::similarityIndex(const std::string& targetUser, const std::string& user_b)
 {
-    double simIndex = 0;
+    double simIndex = 0.0;
     int numOfScores = 0;
 
-    if( unseen_.find(user_a) == unseen_.end() )
-    {
-        for( size_t i = 0; i < videos_->size(); ++i )
-        {
-            if( (*profiles_)[user_a][i] != 0 && (*profiles_)[user_b][i] != 0 )
-            {
-                if( (*profiles_)[user_a][i] == (*profiles_)[user_b][i] )
-                    simIndex += 1.0;
-                else
-                    simIndex += -1.0;
-                ++numOfScores;
-            }
-            else
-            {
-                if((*profiles_)[user_a][i] == 0)
-                    unseen_[user_a].push_back(i);
-            }
-        }
-    }
-    else
-    {
-        for( size_t i = 0; i < videos_->size(); ++i )
-        {
-            if( (*profiles_)[user_a][i] != 0 && (*profiles_)[user_b][i] != 0 )
-            {
-                if( (*profiles_)[user_a][i] == (*profiles_)[user_b][i] )
-                    simIndex += 1.0;
-                else
-                    simIndex += -1.0;
-                ++numOfScores;
-            }
-        }
-    }
-
+    simIndex = simNScores_[targetUser][user_b][0];
+    numOfScores = int(simNScores_[targetUser][user_b][1]);
+    
     if( numOfScores > 0 )
         simIndex /= numOfScores;
-
+    
     return simIndex;
 }
 
+// SOMEWHAT SLOW
+void RecommendationEngine::seenUnseen(const std::string& targetUser, const std::string& user_b)
+{
+    // caching seen and unseen videos
+    if( unseen_.find(user_b) == unseen_.end() ) // if other user is also not in unseen vector
+    {
+        for( size_t i = 0; i < videos_->size(); ++i )
+        {
+            if( isZero((*profiles_)[targetUser][i]) == false && isZero((*profiles_)[user_b][i]) == false )
+            {
+                seen_[targetUser].push_back(i);
+                seen_[user_b].push_back(i);
+            }
+            else
+            {
+                if( isZero((*profiles_)[targetUser][i]) == true )
+                    unseen_[targetUser].push_back(i);
+                else
+                    seen_[targetUser].push_back(i);
+
+                if( isZero((*profiles_)[user_b][i]) == true )
+                    unseen_[user_b].push_back(i);
+                else
+                    seen_[user_b].push_back(i);
+            }
+        }
+    }
+    else // if target user is not in unseen vector but the other user is
+    {
+        for( size_t i = 0; i < videos_->size(); ++i )
+        {
+            if( isZero((*profiles_)[targetUser][i]) == false )
+                seen_[targetUser].push_back(i);
+
+            else
+                unseen_[targetUser].push_back(i);
+        }    
+    }
+}
+
+void RecommendationEngine::cacheData()
+{
+    std::cout << "\nCalculating and Caching Data..."
+              << "\nLooks like this may take a few hours!\n\n";
+
+    std::ostream& os = std::clog;
+    std::string message = "Progress: #";
+    char symbol = '#';
+    int percentage = 1; 
+    int pct = 0;
+    const int progress_level = 1;
+
+    os << "\r [" << std::setw(3) << percentage << "%] " << message << std::flush;
+    
+    cacheSeenUnseen();
+
+    double remains = 0.0; 
+    if( profiles_->size() >= 100 )
+    {   
+        remains = profiles_->size() / 100;
+        if( profiles_->size() % 100 != 0 )
+            remains += (profiles_->size() % 100) * .01;
+    }
+
+    for( auto& targetUser : *profiles_ )
+    {
+        ++pct;
+           
+        cacheSimNScores(targetUser.first);
+
+        if( profiles_->size() >= 100 )
+        {     
+            if( (pct % int(std::ceil(remains))) == 0 )
+            { 
+                percentage += progress_level; 
+                message += symbol;
+                os << "\r [" << std::setw(3) << percentage << "%] " << message << std::flush;
+            }
+        }
+        else
+        {
+            percentage += progress_level; 
+            message += symbol;
+            os << "\r [" << std::setw(3) << percentage << "%] " << message << std::flush;
+        }
+    }
+
+    os << "\r [" << std::setw(3) << 100 << "%] Progress: ####################################################################################################" << std::flush;
+    std::cout << "\nCalculations and Caching completed!\n";    
+}
+
+void RecommendationEngine::cacheSeenUnseen()
+{
+    auto targetUser = profiles_->begin();
+    if( unseen_.find(targetUser->first) == unseen_.end() )
+    {
+        for( auto& user_b : *profiles_ )
+        {
+            if( user_b.first != targetUser->first )
+            {
+                if( unseen_.find(user_b.first) == unseen_.end() )
+                    seenUnseen(user_b.first, targetUser->first);
+            }
+        }
+    }
+}
+
+// SLOW
+void RecommendationEngine::cacheSimNScores(const std::string& targetUser)
+{
+    std::vector<std::unordered_map<std::string, std::array<double,2>>> splitMaps(4, std::unordered_map<std::string, std::array<double,2>>());
+    
+    const int splits = profiles_->size()/4;
+
+    //profiles_->begin() to endQuad1
+    auto endQuad1 = profiles_->begin();
+    std::advance(endQuad1, splits);
+    //endQuad1 to endQuad2
+    auto endQuad2 = endQuad1;
+    std::advance(endQuad2, splits);
+    //endQuad2 to endQuad3
+    auto endQuad3 = endQuad2;
+    std::advance(endQuad3, splits);
+    //endQuad3 to profiles_->end()
+    
+    if( simNScores_.find(targetUser) == simNScores_.end() )
+    {
+        for( size_t i = 0; i < seen_[targetUser].size(); ++i )
+        {
+            int vidIndex = seen_[targetUser][i];
+
+            std::thread t1(&RecommendationEngine::simNScoresForThreads, this, std::ref(splitMaps[0]), profiles_->begin(), endQuad1, vidIndex, targetUser);
+            std::thread t2(&RecommendationEngine::simNScoresForThreads, this, std::ref(splitMaps[1]), endQuad1, endQuad2, vidIndex, targetUser);
+            std::thread t3(&RecommendationEngine::simNScoresForThreads, this, std::ref(splitMaps[2]), endQuad2, endQuad3, vidIndex, targetUser);
+            std::thread t4(&RecommendationEngine::simNScoresForThreads, this, std::ref(splitMaps[3]), endQuad3, profiles_->end(), vidIndex, targetUser);
+
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        }
+    }
+    
+    std::unordered_map<std::string, std::array<double,2>> finishedMap;
+
+    for( auto& map2Merge : splitMaps )
+        finishedMap.merge(map2Merge);
+
+    simNScores_[targetUser] = finishedMap;
+}
+
+void RecommendationEngine::simNScoresForThreads(std::unordered_map<std::string, std::array<double,2>>& map2Merge, 
+                          std::unordered_map<std::string, std::vector<int>>::iterator begin, 
+                          std::unordered_map<std::string, std::vector<int>>::iterator end, const int vidIndex, const std::string targetUser)
+{
+    double simIndex = 0.0;
+    std::for_each(begin, end, [&](auto& profile)
+    {
+        if( profile.first != targetUser )
+        {
+            if( isZero((*profiles_)[profile.first][vidIndex]) == false )
+            {
+                simIndex = ( isEqual((*profiles_)[targetUser][vidIndex], (*profiles_)[profile.first][vidIndex]) ) ? 1.0 : -1.0;
+                map2Merge[profile.first][1] += 1;
+            }
+            map2Merge[profile.first][0] += simIndex;
+            map2Merge[profile.first][1] += 0;
+        }
+    });
+}
+
+bool RecommendationEngine::isZero(const double simScore)
+{
+    if( simScore == 0.0 )
+        return true;
+    return false;
+}
+
+bool RecommendationEngine::isZero(const int rating)
+{
+    if ( rating == 0 )
+        return true;
+    return false;    
+}
+
+
+
+
+
+
+
+
 int main()
 {
-    Read ReadVidData;
-    std::unordered_map<std::string, std::vector<int>> profiles = ReadVidData.readProfileRatings("data/profileRatings.csv");               
-    std::vector<std::string> videos = ReadVidData.readVidNames("data/youtube_trending_videos_data.csv");
-/* 
+    //Read ReadVidData;
+    std::cout << "\nReading Profile and Ratings data from csv...\n";
+    //std::unordered_map<std::string, std::vector<int>> profiles = ReadVidData.readProfileRatings("data/profileRatings.csv");
+    std::cout << "Reading of Profile and Ratings data from csv done!\n"  
+              << "\nReading video names data from csv...\n";
+    //std::vector<std::string> videos = ReadVidData.readVidNames("data/youtube_trending_videos_data.csv");
+    std::cout << "Reading of video names data from csv done!\n";
+
+    
     std::unordered_map<std::string, std::vector<int>> profiles = { 
                                                     {"Tony",    {0, 1, 1, 0, 0, -1, -1, 1, 0, 1, 0, 1}}, 
                                                     {"Jackson", {0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, -1}},
                                                     {"Rodger",  {0, 1, 0, 1, 0, -1, -1, 0, 0, 0, 1, 1}},
                                                     {"Joe",     {-1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1}},
                                                     {"Charles", {0, -1, 1, 0, 0, 1, 1, 1, 0, 1, 0, -1}},
-                                                    {"David",   {1, 0, 0, -1, 0, -1, 1, 0, 0, -1, 0, -1}} 
+                                                    {"David",   {1, 0, 0, -1, 0, -1, 1, 0, 0, -1, 0, -1}}, 
+                                                    {"Jason",   {-1, 0, 1, -1, 0, 1, 0, -1, 0, -1, 1, -1}},
+                                                    {"Amy",     {0, 0, 0, -1, -1, 0, 0, -1, 1, -1, -1, 1}} 
                                                 };
+                                          
+
 
     std::vector<std::string> videos = {
                                     "1) Zach King's Harry Potter illusion - 2.2 billion views", 
@@ -154,26 +346,11 @@ int main()
                                     "11) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla varius.", 
                                     "12) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque posuere."
                                     };
-
-    std::string name;
-    std::cout << "Enter your username: \n";
-    std::cin >> name;
-    std::vector<int> ratings;
-
-    for(int i = 0; i < 12; ++i)
-    {
-        std::cout << "Rate this video [0: unseen | 1: like | -1: dislike] -> " << videos[i] << "\n";
-        int rating;
-        std::cin >> rating;
-        if(rating != 1 && rating != -1 && rating != 0)
-            exit(0);
-        ratings.push_back(rating);
-    }
-
-    profiles[name] = ratings;
-*/
+    
+    
+    // caching data
     RecommendationEngine Engine(videos, profiles);
-
+    
     for( auto& user : profiles )
     {
         std::cout << "\n#########################################################################################################\n";
@@ -192,11 +369,30 @@ int main()
             
         std::cout << "#########################################################################################################\n";
     }
+    
+    /*
+    // client interaction begins
+    std::string name;
+    std::cout << "\nEnter a username: ";
+    std::cin >> name;
 
-/*
-    std::vector<std::pair<std::string, double>> userRecommendations = Engine.getRecommendations(name,3);
-    std::cout << "\n" << name << ", here are your recommended videos:\n";
-    for( auto& recommended : userRecommendations )
-        std::cout << recommended.first << "\n";
-*/
+    while( name != "" )
+    {
+        while( profiles.find(name) == profiles.end() )
+        {
+            std::cout << "That username is not valid. Please Try Again\n"
+                      << "\nEnter a username: ";
+            std::cin >> name;
+        }
+        std::cout << "Fetching Recommendations...\n";
+     
+        std::vector<std::pair<std::string, double>> userRecommendations = Engine.getRecommendations(name,3);
+        std::cout << "\n" << name << ", here are your recommended videos:\n";
+        for( auto& recommended : userRecommendations )
+            std::cout << recommended.first << "\n";
+
+        std::cout << "\nEnter a username: ";
+        std::cin >> name;
+    }
+    */
 }
--- RecommendationEngine.hpp
+++ RecommendationEngine.hpp
@@ -1,7 +1,7 @@
 /*
 * Author: Tony S-M
 * Date: 12/10/2021
-
+*
 * A RecommendationEngine class
 *
 * Using collaborative filtering
@@ -11,22 +11,39 @@
 #define _RECOMMENDATIONENGINE_HPP_
 
 typedef std::vector<std::pair<std::string, double>> vecOfStrToDoubPairs;
+
 class RecommendationEngine{
 public:
     RecommendationEngine(std::vector<std::string>& videos, std::unordered_map<std::string, std::vector<int>>& profiles) 
-    : videos_(&videos), profiles_(&profiles) {}
+    : videos_(std::make_unique<std::vector<std::string>>(videos)), profiles_(std::make_unique<std::unordered_map<std::string, std::vector<int>>>(profiles)) { cacheData(); }
 
     vecOfStrToDoubPairs getSimilarUsers(const std::string& targetUser, int k = 3);
-    
-    vecOfStrToDoubPairs getRecommendations(const std::string& user, int k, vecOfStrToDoubPairs simUsers = vecOfStrToDoubPairs());
+    vecOfStrToDoubPairs getRecommendations(const std::string& targetUser, int k, vecOfStrToDoubPairs simUsers = vecOfStrToDoubPairs());
     
 private:
-    std::vector<std::string>* videos_;
-    std::unordered_map<std::string, std::vector<int>>* profiles_;
-    std::unordered_map<std::string, std::vector<int>> unseen_;
+    void simNScoresForThreads(std::unordered_map<std::string, std::array<double,2>>& map2Merge, 
+                              std::unordered_map<std::string, std::vector<int>>::iterator begin, 
+                              std::unordered_map<std::string, std::vector<int>>::iterator end, const int vidIndex, const std::string targetUser);
 
-    double getSimilarityIndex(const std::string& user_a, const std::string& user_b);
+    double similarityIndex(const std::string& targetUser, const std::string& user_b);
+    void seenUnseen(const std::string& targetUser, const std::string& user_b);
+
+    void cacheData();
+    void cacheSeenUnseen();
+    void cacheSimNScores(const std::string& targetUser);
+
+    bool isEqual(const int left, const int right) { return left == right; }
+
+    // overloaded methods
+    bool isZero(const int rating);
+    bool isZero(const double simScore);
+
+    std::unique_ptr<std::vector<std::string>> videos_; // points to a vector of video titles
+    std::unique_ptr<std::unordered_map<std::string, std::vector<int>>> profiles_; // points to an unordered map keyed to usernames with values being their ratings
+    
+    std::unordered_map<std::string, std::vector<int>> unseen_; // cache for indices of unseen videos for each user
+    std::unordered_map<std::string, std::vector<int>> seen_; // cache for indices of seen videos for each user
+    std::unordered_map<std::string, std::unordered_map<std::string, std::array<double,2>>> simNScores_; // cache for a map of data for each user
 };
 
 #endif //_RECOMMENDATIONENGINE_HPP_
-
